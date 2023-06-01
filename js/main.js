f4c688eedd188f646a2a6b54a9c7fc8fc0d0a779
Oleh <example@gmail.com>
2023-06-01 22:03:54 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+"use strict"
+
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(8%3);
+
+console.log(2 + 2 * 2 !== '6');
+
+const isChecked = false,
+      isClose = false,
+      isHere = false;
+
+
+console.log(isChecked || isClose || !isHere);
+
+
